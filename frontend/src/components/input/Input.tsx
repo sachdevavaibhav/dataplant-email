988dbf07b743a2c6b8b49b78c92b43e4b25d49aa
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2024-01-12 15:08:38 +0530

    search input and input component

---
+++ frontend/src/components/input/Input.tsx
@@ -0,0 +1,23 @@
+interface IInput {
+    placeholder: string,
+    classes?: string,
+    icon?: string | undefined,
+    onChangeHandler?: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+function Input({placeholder, classes, onChangeHandler, icon  ,...props}: IInput) {
+    classes = "input w-full bg-[#ffffff] placeholder:text-[#999999] text-gray-dark border border-[#E4E4EE] rounded focus:outline-none" + " " + classes;
+    return (
+        <div className="flex font-nunito relative">
+            {
+                icon &&
+                <div className="absolute right-5 top-1/3 w-5">
+                    <img src={icon} aria-hidden={true} className='w-full' />
+                </div>
+            }
+            <input type="text" placeholder={placeholder} className={classes} {...props} />
+        </div>
+    )
+}
+
+export default Input;
--- frontend/src/pages/schedulerDashboard/SchedulerDashboard.tsx
+++ frontend/src/pages/schedulerDashboard/SchedulerDashboard.tsx
@@ -1,6 +1,8 @@
 import Table from "./components/Table";
 import DashboardWrapper from "./components/DashboardWrapper";
+import Input from "../../components/input/Input";
 import { useFetchSchedules } from "./hooks/useFetchSchedules";
+import searchIcon from '../../assets/icons/search.svg';
 
 function SchedulerDashboard() {
     const [schedules] = useFetchSchedules();
@@ -10,6 +12,10 @@ function SchedulerDashboard() {
             <div className="fixed right-0 top-[50px] h-10 w-[calc(100vw-5rem)] bg-[#3C1E5A1A] border border-[#D8D2DE]"></div>
             <DashboardWrapper>
                 <div className="px-8">
+                    <div className="flex justify-between pt-5 mb-8">
+                        <Input placeholder="Search" classes="w-96" icon={searchIcon} onChangeHandler={(e) => console.log(e.target.value)} />
+                        <div>Add</div>
+                    </div>
                     <Table data={schedules} />
                 </div>
             </DashboardWrapper>
